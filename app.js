137edef70f544f5ccf4b1ad97da155a3a1c3b804
Mehdi Sajedi <mehdiisajedi@gmail.com>
2021-07-20 13:27:20 -0700

    added proper tabbing accessibility

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Currently...
 
 // User can begin input with a bunch of 0's followed by a number and it will work
-// Non-number inputs will give error message, but calculation still goes through
-// ^^ Same for entering non-integer for people
 // User can enter more than 2 decimal places. Either disallow entering more than 2 (for bill and custom tip) or let them type it out but then round it on form change. Same general idea for people input but with an integer.
+// Can't tab to radio buttons
+// Safari can't tab to reset
 
 const formEl = document.querySelector('#form');
 
@@ -13,6 +13,7 @@ const peopleEl = document.querySelector('#people');
 
 const customTipEl = document.querySelector('#custom-tip-input');
 
+const tipInputs = document.querySelectorAll('.tip-input');
 const tipLabels = document.querySelectorAll('.tip-label');
 const resetBtn = document.querySelector('#reset-btn');
 
@@ -26,35 +27,44 @@ const totalTipEl = document.querySelector('#tip-amount');
 const totalBillEl = document.querySelector('#total-amount');
 
 let billAmount, tipAmount, peopleAmount;
-let usingCustomTip = false;
+let allowCalculation;
 
-function roundTwoDecimals(e) {
-  let t = e;
-  e =
-    t.indexOf('.') >= 0
-      ? t.substr(0, t.indexOf('.')) + t.substr(t.indexOf('.'), 3)
-      : t;
-  return e;
-}
-
-// Show red border and text if invalid input
+// Form validating / showing error messages
 function validateInput(element, container, inputField) {
-  if (+element.value <= 0 || isNaN(+element.value)) {
-    container.classList.add('show-error');
-  } else {
-    container.classList.remove('show-error');
-  }
+  const delay = setTimeout(() => {
+    if (+element.value <= 0 || isNaN(+element.value)) {
+      container.classList.add('show-error');
+      allowCalculation = false;
+    } else {
+      container.classList.remove('show-error');
+      allowCalculation = true;
+    }
+    // In addition for people input field, check that is an integer
+    if (inputField === 'people' && !Number.isInteger(+element.value)) {
+      container.classList.add('show-error');
+      allowCalculation = false;
+    }
 
-  if (inputField === 'people' && !Number.isInteger(+element.value)) {
-    container.classList.add('show-error');
-  }
+    // Allow custom tip input to be 0
+    if (
+      inputField === 'custom' &&
+      element.value !== '' &&
+      +element.value === 0
+    ) {
+      container.classList.remove('show-error');
+      allowCalculation = true;
+    }
+    clearTimeout(delay);
+  }, 650);
 }
 
-billEl.addEventListener('keyup', (e) => {
+// Bill input
+billEl.addEventListener('input', (e) => {
   billAmount = +e.target.value;
   validateInput(billEl, billContainer);
 });
 
+// Tip inputs
 tipEl.addEventListener('click', (e) => {
   // Click will register on both input and label.
   // If statements used to manipulate classes from the proper elements
@@ -70,37 +80,68 @@ tipEl.addEventListener('click', (e) => {
   }
 });
 
-customTipEl.addEventListener('keyup', (e) => {
-  usingCustomTip = true;
-  tipAmount = +e.target.value;
-  tipLabels.forEach((el) => el.classList.remove('selected'));
-  validateInput(customTipEl, tipContainer);
+// ********************************************************
+
+tipEl.addEventListener('keyup', (e) => {
+  // Click will register on both input and label.
+  // If statements used to manipulate classes from the proper elements
+  if (e.code === 'Space') {
+    if (e.target.classList.contains('tip-input')) {
+      console.log(e.target);
+      tipAmount = +e.target.value;
+      customTipEl.value = '';
+      tipContainer.classList.remove('show-error');
+      tipInputs.forEach((el) => el.classList.remove('selected'));
+      e.target.classList.add('selected');
+    }
+  }
 });
 
-peopleEl.addEventListener('keyup', (e) => {
+// ********************************************************
+
+// Custom tip input
+customTipEl.addEventListener('input', (e) => {
+  tipAmount = +e.target.value;
+  tipLabels.forEach((el) => el.classList.remove('selected'));
+  validateInput(customTipEl, tipContainer, 'custom');
+});
+
+// People input
+peopleEl.addEventListener('input', (e) => {
   peopleAmount = +e.target.value;
   validateInput(peopleEl, peopleContainer, 'people');
 });
 
+// Form change
 formEl.addEventListener('change', () => {
+  if (!allowCalculation) return;
+
   const totalTip = ((billAmount * tipAmount) / 100 / peopleAmount).toFixed(2);
   const totalBill = (
     (billAmount * (1 + tipAmount / 100)) /
     peopleAmount
   ).toFixed(2);
 
-  if (isNaN(totalTip || totalBill) || peopleAmount === 0) return;
+  if (isNaN(totalTip || totalBill)) return;
+  if (totalTip == Infinity || totalBill == Infinity) return;
 
   totalTipEl.innerText = `$${totalTip}`;
   totalBillEl.innerText = `$${totalBill}`;
 });
 
-resetBtn.addEventListener('click', () => {
+// Reset button
+resetBtn.addEventListener('click', (e) => {
   tipLabels.forEach((el) => el.classList.remove('selected'));
-  billAmount = 0;
-  tipAmount = 0;
-  peopleAmount = 1;
+  billAmount = null;
+  tipAmount = null;
+  peopleAmount = null;
 
-  totalTipEl.innerText = '$0';
-  totalBillEl.innerText = '$0';
+  billContainer.classList.remove('show-error');
+  tipContainer.classList.remove('show-error');
+  peopleContainer.classList.remove('show-error');
+
+  totalTipEl.innerText = '$0.00';
+  totalBillEl.innerText = '$0.00';
+
+  e.target.blur();
 });
