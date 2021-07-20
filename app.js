46bf93efb55b40c09147041622f514a7a28c286b
Mehdi Sajedi <mehdiisajedi@gmail.com>
2021-07-19 20:25:23 -0700

    working app - needs more error handling/form validation

--- app.js
+++ app.js
@@ -1,39 +1,85 @@
+// Currently...
+
+// User can begin input with a bunch of 0's followed by a number and it will work
+// Non-number inputs will give error message, but calculation still goes through
+// ^^ Same for entering non-integer for people
+// User can enter more than 2 decimal places. Either disallow entering more than 2 (for bill and custom tip) or let them type it out but then round it on form change. Same general idea for people input but with an integer.
+
 const formEl = document.querySelector('#form');
 
 const billEl = document.querySelector('#bill');
 const tipEl = document.querySelector('.form__tip__options');
 const peopleEl = document.querySelector('#people');
 
+const customTipEl = document.querySelector('#custom-tip-input');
+
 const tipLabels = document.querySelectorAll('.tip-label');
 const resetBtn = document.querySelector('#reset-btn');
+
+// Error handling
+const billContainer = document.querySelector('#bill-container');
+const tipContainer = document.querySelector('#tip-container');
+const peopleContainer = document.querySelector('#people-container');
 
 // Output
 const totalTipEl = document.querySelector('#tip-amount');
 const totalBillEl = document.querySelector('#total-amount');
 
-let billAmount, tipAmount;
-let peopleAmount = 1;
+let billAmount, tipAmount, peopleAmount;
+let usingCustomTip = false;
+
+function roundTwoDecimals(e) {
+  let t = e;
+  e =
+    t.indexOf('.') >= 0
+      ? t.substr(0, t.indexOf('.')) + t.substr(t.indexOf('.'), 3)
+      : t;
+  return e;
+}
+
+// Show red border and text if invalid input
+function validateInput(element, container, inputField) {
+  if (+element.value <= 0 || isNaN(+element.value)) {
+    container.classList.add('show-error');
+  } else {
+    container.classList.remove('show-error');
+  }
+
+  if (inputField === 'people' && !Number.isInteger(+element.value)) {
+    container.classList.add('show-error');
+  }
+}
 
 billEl.addEventListener('keyup', (e) => {
   billAmount = +e.target.value;
-  console.log(billAmount);
+  validateInput(billEl, billContainer);
 });
 
 tipEl.addEventListener('click', (e) => {
+  // Click will register on both input and label.
+  // If statements used to manipulate classes from the proper elements
   if (e.target.classList.contains('tip-input')) {
     tipAmount = +e.target.value;
-    console.log(tipAmount);
+    customTipEl.value = '';
+    tipContainer.classList.remove('show-error');
   }
 
   if (e.target.classList.contains('tip-label')) {
     tipLabels.forEach((el) => el.classList.remove('selected'));
-    e.target.classList.toggle('selected');
+    e.target.classList.add('selected');
   }
+});
+
+customTipEl.addEventListener('keyup', (e) => {
+  usingCustomTip = true;
+  tipAmount = +e.target.value;
+  tipLabels.forEach((el) => el.classList.remove('selected'));
+  validateInput(customTipEl, tipContainer);
 });
 
 peopleEl.addEventListener('keyup', (e) => {
   peopleAmount = +e.target.value;
-  console.log(peopleAmount);
+  validateInput(peopleEl, peopleContainer, 'people');
 });
 
 formEl.addEventListener('change', () => {
@@ -43,7 +89,7 @@ formEl.addEventListener('change', () => {
     peopleAmount
   ).toFixed(2);
 
-  if (isNaN(totalTip || totalBill)) return;
+  if (isNaN(totalTip || totalBill) || peopleAmount === 0) return;
 
   totalTipEl.innerText = `$${totalTip}`;
   totalBillEl.innerText = `$${totalBill}`;
