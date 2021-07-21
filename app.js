35307c88313fdb02360d60a6048f7a5be6edc1f8
Mehdi Sajedi <mehdiisajedi@gmail.com>
2021-07-21 12:26:11 -0700

    completed

--- app.js
+++ app.js
@@ -1,66 +1,90 @@
-// Currently...
+'use strict';
 
-// User can begin input with a bunch of 0's followed by a number and it will work
-// User can enter more than 2 decimal places. Either disallow entering more than 2 (for bill and custom tip) or let them type it out but then round it on form change. Same general idea for people input but with an integer.
+// * ------------------------------------------------------------------------------------------
 
+// SELECTING ELEMENTS
+
+// Form
 const formEl = document.querySelector('#form');
 
+// Number input fields
 const billEl = document.querySelector('#bill');
-const tipEl = document.querySelector('.form__tip__options');
+const customTipEl = document.querySelector('#custom-tip-input');
 const peopleEl = document.querySelector('#people');
 
-const customTipEl = document.querySelector('#custom-tip-input');
+// Tip group parent (for event delegation)
+const tipEl = document.querySelector('.form__tip__options');
 
+// To remove active class from radio buttons on custom tip input (visually signal the button is no longer selected)
 const tipInputs = document.querySelectorAll('.tip-input');
-// const tipLabels = document.querySelectorAll('.tip-label');
+
+// Reset
 const resetBtn = document.querySelector('#reset-btn');
 
-// Error handling
+// Used to add/remove show-error class
 const billContainer = document.querySelector('#bill-container');
 const tipContainer = document.querySelector('#tip-container');
 const peopleContainer = document.querySelector('#people-container');
 
-// Output
+// Calculator output
 const totalTipEl = document.querySelector('#tip-amount');
 const totalBillEl = document.querySelector('#total-amount');
 
 let billAmount, tipAmount, peopleAmount;
-let allowCalculation;
 
-// Form validating / showing error messages
+// For looping rather than typing each out
+const allContainers = [billContainer, tipContainer, peopleContainer];
+
+// * ------------------------------------------------------------------------------------------
+
+// FORM VALIDATING
+
 function validateNumberInput(element, container, inputField) {
+  // For UX
   const delay = setTimeout(() => {
+    // General validation
     if (+element.value <= 0 || isNaN(+element.value)) {
       container.classList.add('show-error');
-      allowCalculation = false;
     } else {
       container.classList.remove('show-error');
-      allowCalculation = true;
     }
 
-    // For people input, check that is an integer
-    if (inputField === 'people' && !Number.isInteger(+element.value)) {
+    // Input specific validation
+
+    // For bill input, check that it doesn't have more than 2 decimal places
+    if (
+      inputField === 'bill' &&
+      +element.value !== Math.round(+element.value * 100) / 100
+    ) {
       container.classList.add('show-error');
-      allowCalculation = false;
     }
 
-    // Allow custom tip input to be 0
+    // For custom tip input, allow it to be 0
     if (
       inputField === 'custom' &&
       element.value !== '' &&
       +element.value === 0
     ) {
       container.classList.remove('show-error');
-      allowCalculation = true;
     }
+
+    // For people input, check that is an integer
+    if (inputField === 'people' && !Number.isInteger(+element.value)) {
+      container.classList.add('show-error');
+    }
+
     clearTimeout(delay);
-  }, 1000);
+  }, 400);
 }
+
+// * ------------------------------------------------------------------------------------------
+
+// EVENT LISTENERS
 
 // Bill input
 billEl.addEventListener('input', (e) => {
   billAmount = +e.target.value;
-  validateNumberInput(billEl, billContainer);
+  validateNumberInput(billEl, billContainer, 'bill');
 });
 
 // Tip inputs
@@ -83,13 +107,18 @@ customTipEl.addEventListener('input', (e) => {
 
 // People input
 peopleEl.addEventListener('input', (e) => {
-  peopleAmount = +e.target.value;
+  peopleAmount = e.target.value;
   validateNumberInput(peopleEl, peopleContainer, 'people');
 });
 
 // Form change
 formEl.addEventListener('change', () => {
-  if (!allowCalculation) return;
+  if (
+    allContainers.some((container) =>
+      container.classList.contains('show-error')
+    )
+  )
+    return;
 
   const totalTip = ((billAmount * tipAmount) / 100 / peopleAmount).toFixed(2);
   const totalBill = (
@@ -97,6 +126,7 @@ formEl.addEventListener('change', () => {
     peopleAmount
   ).toFixed(2);
 
+  // To prevent updating before user has filled out the inputs
   if (isNaN(totalTip || totalBill)) return;
   if (totalTip == Infinity || totalBill == Infinity) return;
 
@@ -106,13 +136,13 @@ formEl.addEventListener('change', () => {
 
 // Reset button
 resetBtn.addEventListener('click', (e) => {
-  billAmount = null;
-  tipAmount = null;
-  peopleAmount = null;
+  billAmount = NaN;
+  tipAmount = NaN;
+  peopleAmount = NaN;
 
-  billContainer.classList.remove('show-error');
-  tipContainer.classList.remove('show-error');
-  peopleContainer.classList.remove('show-error');
+  allContainers.forEach((container) =>
+    container.classList.remove('show-error')
+  );
 
   totalTipEl.innerText = '$0.00';
   totalBillEl.innerText = '$0.00';
